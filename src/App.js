066632b81c0a5bed8d8e804e6b25ee90a9345c85
David Layman <davidlayman85@gmail.com>
2018-07-21 12:25:39 -0400

    displays a board

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import board from './assets';
 
 class App extends Component {
+ constructor() {
+    super();
+    this.state = {
+      board: new board(),
+    };
+	this.state.board.setBoard(4,6);
+ }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+	  <div className="header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        I'm keeping the logo, we could use it lol.
       </div>
+	  <div className="board">
+	  {/*
+	  this logic was a pain...
+	  our board.board is an array of rows that hold tiles,
+	  so this is a map of board.board, and for each of those arrays, I had to
+	  call a second map for each tile. The tile.color is added into the className,
+	  so any edits to that need done in the css file.
+	  
+	  this could be abstracted, and should be in the future, so the main App
+	  file isn't so messy.
+	  */}
+	  {this.state.board.board.map(row =>{
+	   return  (
+	   <div className="row">
+	   {row.map(tile => {
+		   return(
+	   <div key={Math.random(Date.now())} className={`tile ${tile.color}`}></div>
+	   )})}
+	   </div>
+	   );
+		  }
+		 )
+	  }
+	  </div>
+	  </div>
     );
   }
 }
---
+++ src/assets.js
@@ -0,0 +1,73 @@
+//there's a lot going on here. This creates a store of information,
+//and stores it in board (note in main logic, the address will be: this.state.board.board)
+
+export default class board {
+constructor(){
+	this.board=[];
+}
+
+// method to populate the board with key:value pairs, based on width,height paramaters
+setBoard(height,width) {
+let color = 'w';
+	for(let i=0;i<height;i++){
+	let row =[];
+		for(let j=0;j<width;j++){
+			//if the board has an even number of squares per row, the checker pattern
+			//is: w,b,w,b
+			//    b,w,b,w
+			//while an odd number of squares per row 
+			//is: w,b,w,b,w
+			//	  b,w,b,w,b
+			//
+			//Take note of the last element of each side; and even array looks like:
+			// w,b,w,b,b,w,b,w
+			//while an odd array would look like:
+			//w,b,w,b,w,b,w,b,w,b
+			//
+			//A lot of this logic is using if/else statments to reverse the color at the
+			//correct time, depending on the first if statment "if(width%2===1){..."
+			//
+			//This is a lot of notes, but will be important to remember if the central logic
+			//needs changed, so we don't go through and make the checker pattern not work.
+			if(width%2===1){
+			row.push(new Tile(j,color));
+			if(color==='w'){
+			color='b';	
+			}
+			else{
+			color='w';
+			}
+			}
+			else{
+				if(j===0){
+					if(color==='w'){
+					color='b';	
+					}
+					else {
+					color='w';
+					}
+				}
+			row.push(new Tile(j,color));
+			if(color==='w'){
+			color='b';	
+			}
+			else{
+			color='w';
+			}
+			}
+		}
+	this.board.push(row);
+	}
+}
+
+};
+
+//the new Tile's that are pushed in the main logic are created here.
+//if we need to store another paramater on the tiles, it needs added on here as well as
+//in the main logic.
+class Tile {
+  constructor (tile, color) {
+	this.tile = tile;
+    this.color = color;
+  }
+}
